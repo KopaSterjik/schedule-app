844e6105ffabf47786bf8701b23b077828657743
Kopa Sterjik <linegod8@gmail.com>
2025-09-18 16:55:44 +0300

    finsh header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,9 +7,10 @@ import {
   TouchableOpacity,
   Modal,
   FlatList,
+  Image,
 } from "react-native";
 import React, { useState } from "react";
-import ListItem from "./ListItem";
+import Ionicons from "@expo/vector-icons/Ionicons";
 export default function Header() {
   const [selected, setselected] = useState("Яна");
   const [visible, setvisible] = useState(false);
@@ -24,11 +25,17 @@ export default function Header() {
   return (
     <View style={styles.contanier}>
       <View style={styles.flexcontanier}>
-        <Text style={{ fontSize: 34 }}>10114122</Text>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/Unknown.png")}
+        />
+        <Text style={{ fontSize: 29 }}>10114122</Text>
         <TouchableOpacity
           style={styles.button}
           onPress={() => setvisible(true)}>
-          <Text style={styles.buttontext}>{selected}</Text>
+          <Ionicons style={styles.button} name="people-outline">
+            {selected}
+          </Ionicons>
         </TouchableOpacity>
         <Modal transparent visible={visible} animationType="fade">
           <TouchableOpacity
@@ -38,9 +45,7 @@ export default function Header() {
               <FlatList
                 data={options}
                 renderItem={({ item }) => (
-                  <TouchableOpacity
-                    style={styles.listitem}
-                    onPress={() => handleselect(item)}>
+                  <TouchableOpacity onPress={() => handleselect(item)}>
                     <Text style={styles.listitemtext}>{item}</Text>
                   </TouchableOpacity>
                 )}
@@ -57,19 +62,42 @@ const styles = StyleSheet.create({
     top: 10,
     width: "100%",
     height: 50,
-    backgroundColor: "red",
   },
   flexcontanier: {
     flexDirection: "row",
     justifyContent: "space-around",
+    alignItems: "center",
   },
-  button: {},
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 15,
+  },
+  button: {
+    alignItems: "center",
+    borderRadius: 10,
+    fontSize: 20,
+  },
+
   buttontext: {},
   overlay: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
-  list: {},
-  listitem: {},
-  listitemtext: {},
+  list: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+    elevation: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    width: "50%",
+  },
+  listitemtext: {
+    fontSize: 20,
+  },
 });
