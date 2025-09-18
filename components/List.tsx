ea58708568398a7546d16021ccbdbf9f96f20a57
Kopa Sterjik <linegod8@gmail.com>
2025-09-18 21:52:07 +0300

    add json files with schedule

--- components/List.tsx
+++ components/List.tsx
@@ -1,14 +1,15 @@
 import { Text, View, StyleSheet } from "react-native";
-import React from 'react';
+import React from "react";
 export default function ListItem() {
   return (
-    <View>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.main}>
+      <></>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  main:{
-    
-  }
-})
+  main: {
+    top: 20,
+    backgroundColor: "blue",
+  },
+});
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,10 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
 import React from "react";
 import Header from "../components/Header";
+import List from "../components/List";
+import ListItem from "../components/List";
 export default function Main() {
   return (
     <View>
       <Header />
+      <ListItem />
     </View>
   );
 }
